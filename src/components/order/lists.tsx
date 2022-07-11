f0e55e8037000a397f145bd7a73febc24b73b1f4
Gy <406631892@qq.com>
2022-07-11 23:31:11 +0800

    order页面滚动加载

--- src/components/order/lists.tsx
+++ src/components/order/lists.tsx
@@ -3,6 +3,7 @@ import { Skeleton } from "antd-mobile";
 import s from "./index.less";
 import Item from "./item";
 import { House } from "../home/hot";
+import ShowLoading from "../ShowLoading";
 
 type Order = House & {
   createTime: string;
@@ -11,26 +12,28 @@ type Order = House & {
 export interface Props {
   orders?: Array<Order>;
   type: 0 | 1;
+  showLoading?: boolean;
 }
 
 const Lists: FC<Props> = (props) => {
-  const { orders, type } = props;
+  const { orders, type, showLoading } = props;
   return (
     <div>
       {!orders || orders.length === 0 ? (
-        <>
+        <div className={s.tab_lists}>
           <Skeleton animated className={s.customSkeleton} />
           <Skeleton animated className={s.customSkeleton} />
           <Skeleton animated className={s.customSkeleton} />
           <Skeleton animated className={s.customSkeleton} />
           <Skeleton animated className={s.customSkeleton} />
           <Skeleton animated className={s.customSkeleton} />
-        </>
+        </div>
       ) : (
         <div className={s.tab_lists}>
           {orders!.map((item) => {
             return <Item type={type} key={item.id} {...item} />;
           })}
+          <ShowLoading isLoading={showLoading} loadingId="mk-loading" />
         </div>
       )}
     </div>
--- src/pages/order/index.tsx
+++ src/pages/order/index.tsx
@@ -1,28 +1,68 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Tabs } from "antd-mobile";
-import { useHttpHook } from "@/hooks";
+import { useHttpHook, useObserverHook } from "@/hooks";
 import { CommonEnum } from "@/enums";
 import Lists from "../../components/order/lists";
 import "./index.less";
+import { Http } from "@/utils";
 
 interface Props {}
 
 const User: FC<Props> = () => {
   const [page, setPage] = useState(CommonEnum.PAGE);
-  const [orders] = useHttpHook({
-    url: "/order/lists",
-    body: {
-      ...page,
-    },
+  const [orders, setOrders] = useState<any[]>([]);
+  const [showLoading, setShowLoading] = useState(true);
+  const [type, setType] = useState(0);
+
+  const invokeHttp = async (pageNum: number) => {
+    const result = await Http({
+      url: "/order/lists",
+      body: {
+        ...page,
+        pageNum,
+        type,
+      },
+    });
+    return result;
+  };
+  const fetchOrder = async (pageNum: number) => {
+    const result: any = await invokeHttp(pageNum);
+    if (result && result.length === page.pageSize) {
+      setOrders(result);
+      setShowLoading(true);
+    } else {
+      setShowLoading(false);
+    }
+  };
+
+  useObserverHook("#" + CommonEnum.LOADING_ID, async (entries) => {
+    console.log(entries);
+    if (entries[0].isIntersecting) {
+      const result: any = await invokeHttp(page.pageNum + 1);
+      if (orders && result && result.length === page.pageSize) {
+        setOrders([...orders, ...result]);
+        setPage({
+          ...page,
+          pageNum: page.pageNum + 1,
+        });
+        setShowLoading(true);
+      } else {
+        setShowLoading(false);
+      }
+    }
   });
-  console.log(orders);
+
+  useEffect(() => {
+    fetchOrder(1);
+  }, [type]);
+
   return (
     <Tabs>
       <Tabs.Tab title="未支付" key="unpaid">
-        <Lists orders={orders} type={0} />
+        <Lists orders={orders} type={0} showLoading={showLoading} />
       </Tabs.Tab>
       <Tabs.Tab title="已支付" key="paid">
-        <Lists orders={orders} type={1} />
+        <Lists orders={orders} type={1} showLoading={showLoading} />
       </Tabs.Tab>
     </Tabs>
   );
